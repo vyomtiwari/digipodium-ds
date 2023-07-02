72091df06f65afa9efe32e7cc8b309f0911176f6
vyom <vyomtiwari2019@gmail.com>
2023-07-02 12:36:05 +0530

    condtionals

---
+++ t1.py
@@ -0,0 +1,14 @@
+from turtle import *
+speed('slowest') # slowest, slow, normal, fast,
+print("hello world")
+for i in range (6):
+    fd(150)
+    lt(60)
+fd(150)
+n = 5
+while(n>=0):
+    fd(150)
+    lt(60)
+    n = n-1
+hideturtle() #hides the turtle
+mainloop() # this loop holds the screen that shows turtle
